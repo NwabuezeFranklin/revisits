aaf476ba2061c3b19a24442a8e1c8a73ccc65213
Nwabueze Franklin <nwaframk2017@gmail.com>
2022-05-10 20:01:13 +0100

    Prints prime numbers

---
+++ go.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * just a test
+ * to see if it works
+ */
+int main(void)
+{
+	int i, g, f;
+	printf("we just started, next will be for scanf");
+
+	for(i = 0; i <= 100000; i++)
+	{
+		if(i % 2 == 0)
+			continue;
+		else if(i % 3 == 0)
+			continue;
+		else if(i % 5 == 0)
+			continue;
+		else if(i % 7 == 0)
+			continue;
+
+		else
+		{
+			printf("%d is a prime number", i);
+		}
+		putchar('\n');
+		
+	}
+	printf("it was hard lmao, gugugaga");
+	return 0;
+}
